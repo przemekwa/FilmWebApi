28d17b54c27d46527b686868a8bed97f3e136d69
Przemek <przemekworld@gmail.com>
2015-08-06 19:04:11 +0200

    Jeszcze dużo problemów z Eclipse - Git

---
+++ src/api/filmweb/tests/FilmWebApiTests.java
@@ -0,0 +1,14 @@
+package api.filmweb.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FilmWebApiTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
